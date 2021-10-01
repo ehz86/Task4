9c2286d913a376f534385f9cc35370045d247dc6
ehz86 <hohloff.jegor@yandex.ru>
2021-10-01 11:21:05 +0300

    Add homework files

--- p4.py
+++ p4.py
@@ -1,9 +1,10 @@
-years = int(input("Количество лет:"))
-exhibits = int(input("Количество просмотренных экспонатов: "))
-minutes = exhibits*5
+years = int(input("Количество лет: "))
+exhibits = years*365*96
+print("Кол-во экспонатов: "+ str(exhibits))
+exhibits2 = int(input("Количество просмотренных экспонатов: "))
+minutes = exhibits2*5
 hours = minutes/60
 days = hours/8
 years2 = days/365
 print("Кол-во лет: " + str(years2))
-exhibits2 = years*365*96
-print("Кол-во экспонатов"+ str(exhibits2))
+
